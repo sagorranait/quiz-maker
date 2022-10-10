22fbcca8f2e7937b21e356b50c8ed72259d4ce00
Sagor Rana <sagorranait@gmail.com>
2022-10-11 01:51:16 +0600

    Design of FAQ Section is done.

---
+++ src/Layouts/Title/QuizMakerTitle.js
@@ -0,0 +1,12 @@
+import './QuizMakerTitle.css';
+
+const QuizMakerTitle = ({title, description}) => {
+  return (
+    <div className='quizMaker-title text-center'>
+        <h2 className='fw-bold'>{title}</h2>
+        <p>{description}</p>
+    </div>
+  )
+}
+
+export default QuizMakerTitle
--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -1,8 +1,53 @@
+import { Accordion, Container } from 'react-bootstrap'
+import QuizMakerTitle from '../../Layouts/Title/QuizMakerTitle'
 import './Faq.css'
 
 const Faq = () => {
   return (
-    <div>Faq</div>
+    <Container>
+      <QuizMakerTitle 
+        title='Frequently Asked Questions' 
+        description='Here are some common questions about Reactjs.' 
+      />
+      <Accordion>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>What is the purpose of a react router?</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>How does context api work?</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>What is useRef ?</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </Container>    
   )
 }
 
