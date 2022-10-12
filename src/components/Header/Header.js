202a3fd9aae6f32dd5b5a0f092ded5c0c1dab845
Sagor Rana <sagorranait@gmail.com>
2022-10-12 19:53:17 +0600

    Active link problem has been solved.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,25 @@
 import { Container, Nav, Navbar } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink, useLocation, useParams } from 'react-router-dom'
 import './Header.css'
 
 const Header = () => {
+  let location = useLocation();
+  let { quizId } = useParams();
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className='py-2'>
       <Container>
         <Navbar.Brand>
-            <img alt="logo" src="../images/logo.png" width="30" height="30" className="d-inline-block align-top"/>
-            <span className='ps-2 fw-bold'>QuizMaker</span>
+            <Link to='/'>
+              <img alt="logo" src="../images/logo.png" width="30" height="30" className="d-inline-block align-top"/>
+              <span className='ps-2 fw-bold'>QuizMaker</span>
+            </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <NavLink 
-                className={`nav-links px-3 py-3 text-decoration-none text-white ${({isActive})=> isActive ? 'active': ''}`} 
+                className={`nav-links px-3 py-3 text-decoration-none text-white ${({isActive})=> isActive ? 'active': ''} ${(location.pathname === `/quizzes/${quizId}`) ? 'active': ''}`} 
                 to='/'
                 end
             >
